993c817d0c8e564a0ef8efb96e7967c6dc684805
nbidal18 <nizarbidal18@gmail.com>
2024-05-07 15:03:56 +0200

    sync

--- client.c
+++ client.c
@@ -6,37 +6,11 @@
 /*   By: nbidal <nbidal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/19 14:22:55 by nbidal            #+#    #+#             */
-/*   Updated: 2024/05/03 17:28:19 by nbidal           ###   ########.fr       */
+/*   Updated: 2024/05/07 15:03:53 by nbidal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
-
-void	send_signals(int pid, char *message)
-{
-	int	i;
-	int	j;
-
-	i = 0;
-	while (message[i] != '\0')
-	{
-		j = -1;
-		while (++j < 8)
-		{
-			if (((unsigned char)(message[i] >> (7 - j)) & 1) == 0) // this bugs me a little bit, the "& 1" simply extracts the last bit and compares it 
-				kill(pid, SIGUSR1);
-			else if (((unsigned char)(message[i] >> (7 - j)) & 1) == 1)
-				kill(pid, SIGUSR2);
-			usleep(50); // why do we use usleep()? instead of I guess sleep()?
-		}
-		i++;
-	}
-	while (j++ < 8)
-	{
-		kill(pid, SIGUSR1);
-		usleep(50);
-	}
-}
 
 int	ft_atoi(const char *str)
 {
@@ -65,30 +39,58 @@ int	ft_atoi(const char *str)
 	return (result);
 }
 
-int	main(int argc, char *argv[])
+void	send_signals(int pid, char *message)
 {
-	int		server_pid;
-	char	*message;
+	int				letter;
+	int				i;
+
+	letter = 0;
+	while (message[letter])
+	{
+		i = -1;
+		while (++i < 8)
+		{
+			if (((unsigned char)(message[letter] >> (7 - i)) & 1) == 0)
+				kill(pid, SIGUSR1);
+			else if (((unsigned char)(message[letter] >> (7 - i)) & 1) == 1)
+				kill(pid, SIGUSR2);
+			usleep(50);
+		}
+	letter++;
+	}
+	i = 0;
+	while (i++ < 8)
+	{
+		kill(pid, SIGUSR1);
+		usleep(50);
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	char				*message;
+	int					server_id;
 
 	if (argc == 3)
 	{
-		server_pid = atoi(argv[1]);
-		if (server_pid == 0)
+		server_id = ft_atoi(argv[1]);
+		if (!server_id)
 		{
-			printf("[ERROR] Wrong PID.\n");
+			printf("[ERROR]. Wrong arg");
 			return (0);
 		}
 		message = argv[2];
-		if (message[0] == '\0')
+		if (message[0] == 0)
 		{
-			printf("[ERROR] Insert some text.\n");
+			printf("Tu n'as envoyé aucun texte ! Ecris qqch pls :)");
 			return (0);
 		}
-		send_signals(server_pid, message);
+		send_signals(server_id, message);
 	}
-	else if (argc > 3)
-		printf("[ERROR] Too many arguments.\ntry: ./client <pid> <message>\n");
-	else if (argc < 3)
-		printf("[ERROR] Too few arguments.\ntry: ./client <pid> <message>\n");
+	else
+	{
+		printf("[ERROR]. Too much or too few arguments.\n Make sure ");
+		printf("you enter arguments as follow: ./client <PID> <MESSAGE>");
+	}
 	return (0);
 }
--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: nbidal <nbidal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/24 16:45:55 by nbidal            #+#    #+#             */
-/*   Updated: 2024/05/03 17:28:13 by nbidal           ###   ########.fr       */
+/*   Updated: 2024/05/07 15:03:48 by nbidal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,63 +42,78 @@ char	*ft_strdup(const char *s1)
 	return (str);
 }
 
-int	
+int	ft_recursive_power(int nb, int power)
+{
+	int	res;
 
-char	*letter_to_string(char const *str, char const result)
+	if (power == 0)
+		return (1);
+	else if (power < 0)
+		return (0);
+	else
+	{
+		res = nb * ft_recursive_power(nb, power - 1);
+		return (res);
+	}
+}
+
+char	*letter_to_string(char const *s1, char const letter)
 {
 	int		i;
 	int		j;
-	char	*tmp_str;
+	char	*tab;
 
 	i = 0;
 	j = 0;
-	tmp_str = malloc((ft_strlen(str) + 2) * sizeof(char));
-	if (tmp_str == NULL)
+	tab = malloc((ft_strlen(s1) + 2) * sizeof(char));
+	if (!tab)
 		return (NULL);
+	while (s1[i])
+		tab[j++] = s1[i++];
 	i = 0;
-	tmp_str[j++] = result;
-	tmp_str[j] = '\0';
-	free ((void *)(str));
-	return (tmp_str);
+	tab[j++] = letter;
+	tab[j] = 0;
+	free ((void *)(s1));
+	return (tab);
 }
 
 void	signal_handler(int signum)
 {
-	static int	i; // why are these static?
+	static int	counter;
 	static int	result;
 	static int	len;
-	static char	*str;
+	static char	*final;
 
-	i = 0;
+	counter = 0;
 	result = 0;
 	len = 0;
-	if (str == NULL)
-		str = ft_strdup("");
+	if (!final)
+		final = ft_strdup("");
 	if (signum == SIGUSR1)
-		result = result + 0; // does this turn it into a char or what bruh *** // I guess result is the result we get once we receive all 8 bits
+		result = result + 0;
 	else if (signum == SIGUSR2)
-		result = result + (1 * ft_recursive_power(2, 7 - i)); // what the hell is this
-	i++;
-	if (i == 8)
+		result = result + (1 * ft_recursive_power(2, 7 - counter));
+	counter++;
+	if (counter == 8)
 	{
-		str = letter_to_string(str, result); // how come letter_to_string() is going to receive result as a char when it has been declaered as an int? ***
+		final = letter_to_string(final, result);
 		if (result == '\0')
 		{
-			printf("%s\n", str);
-			str = NULL;
+			printf("%s\n", final);
+			final = NULL;
 		}
-		i = 0;
+		counter = 0;
 		result = 0;
 		len += 1;
 	}
 }
 
-int	main(void) // I really need to understand how both this while and this signal_received struct work
+int	main(void)
 {
 	struct sigaction	signal_received;
-	
-	printf("--> nbidal's server\n");
-	printf("--> PID: %d\n", getpid());
+
+	printf("Welcome to lfabbian's server :-)\n");
+	printf("Server's PID: %d\n", getpid());
 	signal_received.sa_handler = signal_handler;
 	signal_received.sa_flags = 0;
 	sigaction(SIGUSR1, &signal_received, NULL);
